8340925cd60f7340824d620aa0286a9c9bd2f9c2
Aatmik Jain <aatmikjain1803@gmail.com>
2019-06-21 09:41:00 +0530

    ..

--- app/src/main/java/com/example/aatmikjain/memberdirectory/SplashActivity.java
+++ app/src/main/java/com/example/aatmikjain/memberdirectory/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.example.aatmikjain.memberdirectory;
 
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -7,16 +8,19 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.ViewAnimator;
 
 public class SplashActivity extends AppCompatActivity {
 
     private ImageView logo;
+    ViewAnimator animator;
     private static long SPLASH_TIME = 5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         logo = findViewById(R.id.logo);
+        animator = findViewById(R.id.animator2);
         /*new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -27,6 +31,13 @@ public class SplashActivity extends AppCompatActivity {
         },SPLASH_TIME);*/
 
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.splash_animation);
+//        final ValueAnimator valueAnimator=ValueAnimator.ofFloat(0f,1f).setDuration(5000);
+//        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+//            @Override
+//            public void onAnimationUpdate(ValueAnimator animation) {
+//
+//            }
+//        })
         animation.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
@@ -50,7 +61,7 @@ public class SplashActivity extends AppCompatActivity {
 
             }
         });
-        logo.startAnimation(animation);
+        animator.startAnimation(animation);
 
     }
 }
