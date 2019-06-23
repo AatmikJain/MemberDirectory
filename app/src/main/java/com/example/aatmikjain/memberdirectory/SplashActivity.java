4b9dbe6e7032c541355c11535284a0e796b702a6
Aatmik Jain <aatmikjain1803@gmail.com>
2019-06-23 21:06:52 +0530

    ..

--- app/src/main/java/com/example/aatmikjain/memberdirectory/SplashActivity.java
+++ app/src/main/java/com/example/aatmikjain/memberdirectory/SplashActivity.java
@@ -1,12 +1,9 @@
 package com.example.aatmikjain.memberdirectory;
 
-import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.ViewAnimator;
 
